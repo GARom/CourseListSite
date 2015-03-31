5038ec22ab14242c614f2176fccd365a194c304f
Guillermo Romero <garomero1994@yahoo.com>
2015-03-30 20:47:46 -0500

    edited server.js file and finished installing clientside dependencies

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ app.use(stylus.middleware(
 //Routing for stylus
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+if(env === 'development') {
+  mongoose.connect('mongodb://localhost/CourseListSite');
+} else {
+  mongoose.connect('mongodb://garomero:multivision@ds053178.mongolab.com:53178/multivision');
+}
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
@@ -51,6 +55,6 @@ app.get('*', function(req, res) {
   });
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
